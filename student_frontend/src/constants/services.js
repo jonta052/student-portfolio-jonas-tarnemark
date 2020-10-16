98c910dab4b51dddc5ca539d6b341373549cfc53
jonta052 <55479519+jonta052@users.noreply.github.com>
2020-10-16 15:05:03 +0200

    text5

--- student_frontend/src/constants/services.js
+++ student_frontend/src/constants/services.js
@@ -4,14 +4,14 @@ export default [
   {
     id: 1,
     icon: <FaCode className="service-icon" />,
-    title: "web development",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    title: "Webbutveckling",
+    text: `HTML, CSS, .Net framework med MVC`,
   },
   {
     id: 2,
     icon: <FaSketch className="service-icon" />,
-    title: "web design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    title: "Statistik",
+    text: `Kandidatexamen i Statistik`,
   },
   {
     id: 3,
